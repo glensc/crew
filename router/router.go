4aee46209235bef5b6ec4a57c3890fbe47402abf
root <genedna@gmail.com>
2015-06-26 09:26:40 +0800

    Initlization router and signup handler function

--- router/router.go
+++ router/router.go
@@ -2,7 +2,14 @@ package router
 
 import (
 	"github.com/Unknwon/macaron"
+
+	"github.com/containerops/crew/handler"
 )
 
 func SetRouters(m *macaron.Macaron) {
+	//Crew User And Organization V1 API
+
+	m.Group("/w1", func() {
+		m.Post("/signup", handler.W1Signup)
+	})
 }
