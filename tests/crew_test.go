31e8073f79f3db9e0aa947011e9f85562d43ad4b
Meaglith Ma <genedna@gmail.com>
2015-07-21 14:33:39 +0800

    Add comments for all test functions

--- tests/crew_test.go
+++ tests/crew_test.go
@@ -8,90 +8,112 @@ func init() {
 
 }
 
+// POST /v1/token
 func Test_W1PostToken(t *testing.T) {
 
 }
 
+// POST /v1/user
 func Test_W1UserSignup(t *testing.T) {
 
 }
 
+// POST /v1/user/auth
 func Test_W1UserSignin(t *testing.T) {
 
 }
 
+// GET /v1/user/list
 func Test_W1GetUserList(t *testing.T) {
 
 }
 
+// PUT /v1/user/:user/profile
 func Test_W1PutUserProfile(t *testing.T) {
 
 }
 
+// GET /v1/user/:user/profile
 func Test_W1GetUserProfile(t *testing.T) {
 
 }
 
+// POST /v1/user/:user/gravatar
 func Test_W1PostUserGravatar(t *testing.T) {
 
 }
 
+// PUT /v1/user/:user/passwd
 func Test_W1PutUserPasswd(t *testing.T) {
 
 }
 
+// GET /v1/user/:user/organizations
 func Test_W1GetUserOrganizations(t *testing.T) {
 
 }
 
+// GET /v1/user/:user/teams
 func Test_W1GetUserTeams(t *testing.T) {
 
 }
 
+// POST /v1/org
 func Test_W1PostOrganization(t *testing.T) {
 
 }
 
+// PUT /v1/org/:org
 func Test_W1PutOrganization(t *testing.T) {
 
 }
 
+// GET /v1/org/:org
 func Test_W1GetOrganization(t *testing.T) {
 
 }
 
+// DELETE /v1/org/:org
 func Test_W1DeteleOrganization(t *testing.T) {
 
 }
 
+// POST /v1/org/:org/team
 func Test_W1PostTeam(t *testing.T) {
 
 }
 
+// POST /v1/org/:org/team/list
 func Test_W1GetTeams(t *testing.T) {
 
 }
 
+// PUT /v1/org/:org/team/:team
 func Test_W1PutTeam(t *testing.T) {
 
 }
 
+// GET /v1/org/:org/team/:team
 func Test_W1GetTeam(t *testing.T) {
 
 }
 
+// DELETE /v1/org/:org/team/:team
 func Test_W1DeteltTeam(t *testing.T) {
 
 }
 
+// GET /v1/org/:org/team/:team/user/list
 func Test_W1GetTeamUsers(t *testing.T) {
 
 }
 
+// PUT /v1/org/:org/team/:team/:user
 func Test_W1PutTeamUser(t *testing.T) {
 
 }
 
+// DELETE /v1/org/:org/team/:team/:user
 func Test_W1DeleteTeamUser(t *testing.T) {
 
 }
