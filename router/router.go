57d07b895804a3bb6ee1e92767dbd87d89472897
Meaglith Ma <genedna@gmail.com>
2016-09-10 10:03:46 +0800

    Add Team/Organization/Search routers and handler functions.

--- router/router.go
+++ router/router.go
@@ -22,6 +22,31 @@ func SetRouters(m *macaron.Macaron) {
 			m.Delete("/:username", handler.DeleteUserV1Handler)
 
 			m.Post("/:username/password", handler.PostUserPasswordV1Handler)
+			m.Get("/:username/emails", handler.GetUserEmailsV1Handler)
+			m.Post("/:username/emails", handler.PostUserEmailsV1Handler)
+			m.Patch("/:username/emails/default", handler.PatchUserEmailsV1Handler)
+			m.Delete("/:username/emails", handler.DeleteUserEmailsV1Handler)
+		})
+
+		//Organization
+		m.Group("/orgs", func() {
+			m.Post("/", handler.PostOrganizationV1Handler)
+			m.Get("/:org", handler.GetOrganizationV1Handler)
+			m.Patch("/:org", handler.PatchOrganizationV1Handler)
+			m.Delete("/org", handler.DeleteOrganizationV1Handler)
+
+			//Team
+			m.Group("/team", func() {
+				m.Post("/", handler.PostTeamV1Handler)
+				m.Get("/:team", handler.GetTeamV1Handler)
+				m.Patch("/:team", handler.PatchTeamV1Handler)
+				m.Delete("/:team", handler.DeleteTeamV1Handler)
+			})
+		})
+
+		//Search
+		m.Group("/search", func() {
+			m.Get("/users", handler.GetSearchUsersV1Handler)
 		})
 	})
 }
